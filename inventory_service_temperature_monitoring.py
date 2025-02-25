b1ff0f84e3d1ca251fece3880a0fd294f7f73588
cemakpolat <cem.akpolat@sva.de>
2025-02-25 23:50:10 +0100

    feat: Add temperature_monitoring to inventory_service

---
+++ inventory_service_temperature_monitoring.py
@@ -0,0 +1,6 @@
+
+            # Dummy code for temperature monitoring in Inventory Service
+            def get_temperature(drug_id):
+                # Simulate getting temperature from sensor
+                return random.randint(15, 30)  # Degrees Celsius
+            
